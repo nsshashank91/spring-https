7d0dc0eaa43952f00033fbce322678c0dc13cd97
Shashank S <shashank@softedgesolution.com>
2021-05-29 16:33:36 +0530

    first commit

---
+++ src/main/java/com/shash/MyController.java
@@ -0,0 +1,13 @@
+package com.shash;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "hello";
+    }
+}
---
+++ src/main/java/com/shash/SpringHttpsApplication.java
@@ -0,0 +1,34 @@
+package com.shash;
+
+import org.apache.catalina.connector.Connector;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class SpringHttpsApplication {
+
+	@Value("${http.port}")
+	private int httpPort;
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHttpsApplication.class, args);
+	}
+
+	@Bean
+	public ServletWebServerFactory servletContainer() {
+		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+		tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+		return tomcat;
+	}
+
+	private Connector createStandardConnector() {
+		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+		connector.setPort(httpPort);
+		return connector;
+	}
+
+}
